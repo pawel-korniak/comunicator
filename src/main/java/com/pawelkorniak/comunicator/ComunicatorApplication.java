46125c4f8bbeb00dcbcc46a6f87552cde543728d
Paweł Korniak <pawello1020@gmail.com>
2021-08-15 11:44:04 +0200

    initial commit

---
+++ src/main/java/com/pawelkorniak/comunicator/ComunicatorApplication.java
@@ -0,0 +1,13 @@
+package com.pawelkorniak.comunicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComunicatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComunicatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pawelkorniak/comunicator/ComunicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelkorniak.comunicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComunicatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
